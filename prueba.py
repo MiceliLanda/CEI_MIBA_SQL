0ea1f334380d380a10b50d14e2565b06671623e1
MiceliLanda <183381@ids.upchiapas.edu.mx>
2022-03-03 20:28:50 -0600

    new-structOk

--- prueba.py
+++ prueba.py
@@ -13,6 +13,8 @@ def advanceSetence(sentencia):
     #
     nombre = list(entrada.pop())
     nombre.reverse()
+    print('\nNombre de la tabla : ',''.join(reversed(nombre)))
+    print('Pila gramática : ',pila,'\n')
     for i in reversed(nombre):
         if not i.isalnum():
             print('Caracter inválido crack  mira crack-> ',i)
@@ -20,22 +22,17 @@ def advanceSetence(sentencia):
             entrada.append(new)
         else:
             nombre.pop(),pila.pop()
-            print('Entrada Actual : ',' '.join(reversed(nombre)), '  - Verificando : ',i)
+            print('Actual : ',' '.join(reversed(nombre)), '  - Verificando : ',i)
             print(pila)
             if pila.pop() == 'R':
                 pila.extend(list('RL'))
-                print(pila)
-    pila.pop(),pila.pop()
+                if not nombre:
+                    pila.clear(),pila.extend(list('$)TRL('))
+                    print('Pila gramática : ',pila,'\n')
+                else: print('R - > L')
+    # pila.pop(),pila.pop()
     # print('LO QUE SE VA AL firstParent -> ',entrada , pila)
     firstParent(entrada, pila)  
-        
-    # print(new)
-# advanceSetence(('new nombre (atri int) $'.strip(' ').lower().split(' ')))
-""" advanceSetence(('new nombre (atri int, atri2 varchar) $'.strip(' ').lower().split(' ')))
-pila = ['$',',',')','td','name','(']
-entrada = ['varchar)','atri2','int,','atri','(']
-entrada = ['varchar)','atri2','int,','(atri'] """
-
 
 def firstParent(entrada,pila):
     if  len(entrada[-1]) > 1 and pila[-1] == entrada[-1][0]:
@@ -55,14 +52,18 @@ def firstParent(entrada,pila):
 def endParentesis(entrada,pila):
     pila.clear()
     print('final',entrada[-1][-1])
+
     if entrada[-1] == ')':
         print('parentesisCierre suma \n Ejecución Ok')
         entrada.pop()
-        pila.append('$')
+        pila.append('$'),entrada.append('$')
+        print(f'Entrada end : {entrada} Pila end : {pila}')
     elif entrada[-1][-1] == ')':
         entrada.pop()
         entrada.append('$')
+        pila.append('$')
         print('parentesisCierre suma \n Ejecución Ok')
+        print(f'Entrada end : {entrada} Pila end2 : {pila}')
     else: print('ERROR : Se espera un parenteis de cierre')
 
 def checkName(entrada, pila): # atri int, algo bool)
@@ -82,7 +83,7 @@ def checkName(entrada, pila): # atri int, algo bool)
         else:
             name.pop()
             pila.pop()
-            print('Entrada Actual : ',' '.join(reversed(name)), '  - Verificando : ',i)
+            print('Entrada Actual :',''.join(reversed(name)), '  - Verificando : ',i)
             print(pila)
             if pila.pop() == 'R':
                 pila.extend(list('RL'))
@@ -96,54 +97,52 @@ def checkName(entrada, pila): # atri int, algo bool)
         # return entrada, pila
         checkDato(entrada, pila)
     else: print('Indentificador 0')
-        
     # else: tokens['identificadores'] = 0
 def checkSeparador(entrada,pila):
     """ CHECAR QUE AUMENTE EL CONTADOR DE SEPARADOR EN EL DE TOKENS """
 
-    print('Entrada chekSperador : ',entrada, ' Pila check separador : ',pila)
+    print('Entrada chekSperador : ',entrada[-1], ' Pila check separador : ',pila)
     if len(entrada[-1]) > 1 and entrada[-1][-1] == ',': #CUANDO LLEGA CON TIPO DE DATO
         clean = entrada[-1][0:len(entrada[-1])-1]
         entrada.pop(),entrada.append(clean)
-        print('SALI DE SEPARADOR PARA ENTRAR A CHECKDATO -> ',entrada)
+        print('SALI DE SEPARADOR PARA ENTRAR A CHECKDATO 1-> ',entrada)
         checkDato(entrada, pila)
     elif entrada[-1] == ',': #ESTE CASO ES CUANDO LLEGA LA COMA SOLA EJ. ','
         entrada.pop()
-        print('SALI DE SEPARADOR PARA ENTRAR A CHECKDATO -> ',entrada)
+        print('SALI DE SEPARADOR PARA ENTRAR A CHECKDATO 2-> ',entrada)
         checkDato(entrada, pila)
 
-def checkDato(entrada, pila): #int, algo bool)
+def checkDato(entrada, pila): #int, algo bool)5 
     # print('entrada checkDato : ',entrada ,' checkdatoPila : ',pila)
-    print('130 ',entrada[-1][-1::])
-    if (entrada[0] and entrada[0][-1::]) == ')' and len(entrada) <=1:
-        if entrada[-1] in tiposDato:
-            print('eliminarlo nomás -> ', entrada[-1])# TIENE PARENTESIS SOLO EN ENTRADADA.POP
-            entrada.pop(),pila.pop(),endParentesis(entrada, pila)
-        elif entrada[-1][0:len(entrada[-1])-1] in tiposDato: # TIENE PARENTESIS PEGADO AL TIPO DE DATO
-            tipo = entrada[-1][-1::]
-            print('Elif 113 pegado al tipo -> ',entrada[-1])
-            # new = entrada[-1][0:len(entrada[-1])-1
-            endParentesis(entrada, pila)
-        else: 
-            print('No existe el tipo de dato ',entrada[-1][0:len(entrada[-1])-1])
-    elif (entrada[-1] and entrada[-1][-1::]) == ',':    
-        """ CHECAR QUE SE ELIMINE LA PILA TAQMBIÉN """
+    print('Antes del cualquier if de checkdato : ', entrada[-1],'\n')
+    if ',' in entrada[-1]:
+        print('Tiene coma pegado al tipo : ',entrada[-1])
         checkSeparador(entrada, pila)
-    elif entrada[-1] in tiposDato:
-        entrada.pop(),pila.pop()
-        print('Nueva entrada despues de check separador -> ',entrada,'  PIla -> ',pila)
-        if (entrada[-1] or entrada[-1][-1::]) == ',':
-            checkSeparador(entrada, pila)
-        else: 
-            pila.extend(list('TRL'))
-            checkName(entrada, pila)
-    elif entrada[-1].isalnum():
-        pila.extend(list('TRL'))
-        checkName(entrada, pila)
-    else: 
-        print('ERROR: No tiene parentesis de cierre')
+    elif ')' in entrada[-1] and entrada[-1][0:len(entrada[-1])-1] in tiposDato:
+        print('Tiene parentesis, es único : ',entrada[-1])
+        endParentesis(entrada, pila)
+    else:
+        print('No tiene coma ni parentesis de cierre, se checa si es un dato válido : ',entrada[-1])
+        # print('Size entrada: ',len(entrada))
+        if len(entrada) > 1 and len(entrada) <4: # entrada = '$' ')' 'int'
+            # print('Size entrada dentro del if de size: ',len(entrada))
+            if entrada[-1] in tiposDato:
+                print('Se encontró un tipo parecido = ',entrada[-1])
+                entrada.pop(),pila.pop(),pila.extend(list('TRL'))
+                print('size :',len(entrada), entrada)
+                checkName(entrada,pila)
+            else:
+                print('No se encontró un tipo, eliminado y Termina')
+        else:
+            print('QUE ES ENTRADA ',entrada[-1])
+            if entrada[-1] in tiposDato:
+                entrada.pop(),pila.pop(),pila.extend(list('TRL'))
+                print('Entra al else porque no es el ultimo : ',entrada,'\nPIla: ',pila)
+                checkName(entrada,pila)
+            else: 
+                print('Tipo no valido: ' ,entrada[-1])
 
-advanceSetence('new-struct nombre (a int , b double, c varchar )'.strip(' ').lower().split(' '))
+#""" ME QUEDÉ EN VERIFICAR SI EL ULTIMO ELEMENTO DE LA ENTRADA ES SOLO COMA IF 139 """
 
-""" pila, entrada , value = firstParent(['double)','atri3','varchar','atri2','int,','(atri'],['$',',)','T','R','L','('])
-checkName(entrada,pila) """
+advanceSetence('new-struct Alumnos (atri1 int, atri2 bool, atri3 varchar, asd double)'.strip(' ').lower().split(' '))
+
